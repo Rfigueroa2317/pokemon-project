e7a20100259dda57b479fd88dc325504a5701515
ricardofigueroa <rfigueroa2317@gmail.com>
2021-08-09 22:07:41 -0500

    update

--- app.js
+++ app.js
@@ -1,60 +1,82 @@
+function randomPokemon() {
 
+    for (let i = 0; i < Math.floor(Math.random()) * 6; i++) {
 
-for (let i = 0; i < 1; i++) {
+        let pokemon;
 
+        switch (pokemon) {
+            case 1:
+                fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+                    .then(function (ditto) {
+                        return ditto.text();
+                    }).then(function (text) {
+                    console.log("This should be ditto", text);
+                }).catch(function (error) {
+                    console.log('Request failed', error)
+                });
+                break;
 
-    fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-        .then(function (ditto) {
-            return ditto.text();
-        }).then(function (text) {
-        console.log("This should be ditto", text);
-    }).catch(function (error) {
-        console.log('Request failed', error)
-    });
+            case 2:
+                fetch('https://pokeapi.co/api/v2/pokemon/gengar')
+                    .then(function (ditto) {
+                        return ditto.text();
+                    }).then(function (text) {
+                    console.log("This should be Gengar", text);
+                }).catch(function (error) {
+                    console.log('Request failed', error)
+                });
+                break;
 
-    fetch('https://pokeapi.co/api/v2/pokemon/gengar')
-        .then(function (ditto) {
-            return ditto.text();
-        }).then(function (text) {
-        console.log("This should be Gengar", text);
-    }).catch(function (error) {
-        console.log('Request failed', error)
-    });
+            case 3:
+                fetch('https://pokeapi.co/api/v2/pokemon/charizard')
+                    .then(function (ditto) {
+                        return ditto.text();
+                    }).then(function (text) {
+                    console.log("This should be Charizard", text);
+                }).catch(function (error) {
+                    console.log('Request failed', error)
+                });
+                break;
 
-    fetch('https://pokeapi.co/api/v2/pokemon/charizard')
-        .then(function (ditto) {
-            return ditto.text();
-        }).then(function (text) {
-        console.log("This should be Charizard", text);
-    }).catch(function (error) {
-        console.log('Request failed', error)
-    });
+            case 4:
+                fetch('https://pokeapi.co/api/v2/pokemon/slowpoke')
+                    .then(function (ditto) {
+                        return ditto.text();
+                    }).then(function (text) {
+                    console.log("This should be Slowpoke", text);
+                }).catch(function (error) {
+                    console.log('Request failed', error)
+                });
+                break;
 
-    fetch('https://pokeapi.co/api/v2/pokemon/slowpoke')
-        .then(function (ditto) {
-            return ditto.text();
-        }).then(function (text) {
-        console.log("This should be Slowpoke", text);
-    }).catch(function (error) {
-        console.log('Request failed', error)
-    });
+            case 5:
+                fetch('https://pokeapi.co/api/v2/pokemon/machamp')
+                    .then(function (ditto) {
+                        return ditto.text();
+                    }).then(function (text) {
+                    console.log("This should be Machamp", text);
+                }).catch(function (error) {
+                    console.log('Request failed', error)
+                });
+                break;
 
-    fetch('https://pokeapi.co/api/v2/pokemon/machamp')
-        .then(function (ditto) {
-            return ditto.text();
-        }).then(function (text) {
-        console.log("This should be Machamp", text);
-    }).catch(function (error) {
-        console.log('Request failed', error)
-    });
+            case 6:
+                fetch('https://pokeapi.co/api/v2/pokemon/zubat')
+                    .then(function (ditto) {
+                        return ditto.text();
+                    }).then(function (text) {
+                    console.log("This should be Zubat", text);
+                }).catch(function (error) {
+                    console.log('Request failed', error)
+                });
 
-    fetch('https://pokeapi.co/api/v2/pokemon/zubat')
-        .then(function (ditto) {
-            return ditto.text();
-        }).then(function (text) {
-        console.log("This should be Zubat", text);
-    }).catch(function (error) {
-        console.log('Request failed', error)
-    });
-
+                break;
+        }
+    }
+    return pokemon;
 }
+
+
+
+console.log(randomPokemon());
+
