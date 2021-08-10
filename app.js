b53d5cebcb87407e0f6a4c38b0c780d865b246d0
ricardofigueroa <rfigueroa2317@gmail.com>
2021-08-09 21:15:07 -0500

    update

--- app.js
+++ app.js
@@ -1,7 +1,53 @@
-fetch('https://pokeapi.co/api/v2/pokemon/ditto').then(function (ditto){
+fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+    .then(function (ditto) {
     return ditto.text();
-}).then(function (text){
+}).then(function (text) {
     console.log("This should be ditto", text);
-}).catch(function (error){
+}).catch(function (error) {
+    console.log('Request failed', error)
+});
+
+fetch('https://pokeapi.co/api/v2/pokemon/gengar')
+    .then(function (ditto) {
+        return ditto.text();
+    }).then(function (text) {
+    console.log("This should be Gengar", text);
+}).catch(function (error) {
+    console.log('Request failed', error)
+});
+
+fetch('https://pokeapi.co/api/v2/pokemon/charizard')
+    .then(function (ditto) {
+        return ditto.text();
+    }).then(function (text) {
+    console.log("This should be Charizard", text);
+}).catch(function (error) {
+    console.log('Request failed', error)
+});
+
+fetch('https://pokeapi.co/api/v2/pokemon/slowpoke')
+    .then(function (ditto) {
+        return ditto.text();
+    }).then(function (text) {
+    console.log("This should be Slowpoke", text);
+}).catch(function (error) {
+    console.log('Request failed', error)
+});
+
+fetch('https://pokeapi.co/api/v2/pokemon/machamp')
+    .then(function (ditto) {
+        return ditto.text();
+    }).then(function (text) {
+    console.log("This should be Machamp", text);
+}).catch(function (error) {
+    console.log('Request failed', error)
+});
+
+fetch('https://pokeapi.co/api/v2/pokemon/zubat')
+    .then(function (ditto) {
+        return ditto.text();
+    }).then(function (text) {
+    console.log("This should be Zubat", text);
+}).catch(function (error) {
     console.log('Request failed', error)
 });
